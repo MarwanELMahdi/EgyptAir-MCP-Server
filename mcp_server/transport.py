bb83b5dc0397fbf4fac63de4f62200edef26aaab
yousef <yousefhatem730@gmail.com>
2026-07-31 15:36:19 +0300

    transport layer

---
+++ mcp_server/transport.py
@@ -0,0 +1,11 @@
+
+from app import mcp
+
+import tools.flight_tools
+import tools.booking_tools
+import tools.compensation_tools
+import tools.report_tools
+import resources ,sampling
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="127.0.0.1", port=9000)
